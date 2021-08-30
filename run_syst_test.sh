cbae19a399fc95e1f6499812887f2b5b7177b11e
Irene Dutta <irene.dutta@cern.ch>
2021-08-30 16:35:07 -0700

    remove data operations while running syst

--- run_syst_test.sh
+++ run_syst_test.sh
@@ -1,13 +1,13 @@
 #outputname no/yesforsyst systname no/yestrigsys no/yesPNetSFsys
 #note the "nominal" here is essential and this format is used in the command used to make histograms for datacards 
-source process.sh yield_AN_sr_sys_0819_nominal_nosys no nominal no no
-source process.sh yield_AN_sr_sys_0819_JER_Up no JER_Up no no
-source process.sh yield_AN_sr_sys_0819_JER_Down no JER_Down no no
-source process.sh yield_AN_sr_sys_0819_JES_Up no JES_Up no no
-source process.sh yield_AN_sr_sys_0819_JES_Down no JES_Down no no
-source process.sh yield_AN_sr_sys_0819_JMR_Up no JMR_Up no no
-source process.sh yield_AN_sr_sys_0819_JMR_Down no JMR_Down no no
-source process.sh yield_AN_sr_sys_0819_JMS_Up no JMS_Up no no
-source process.sh yield_AN_sr_sys_0819_JMS_Down no JMS_Down no no
-source process.sh yield_AN_sr_sys_0819_nominal yes nominal no yes
-#source process.sh yield_AN_sr_sys_0819_trig_nominal yes nominal yes no
+source process.sh yield_AN_sr_sys_0830_nominal_nosys no nominal no no
+source process_noData.sh yield_AN_sr_sys_0830_JER_Up no JER_Up no no
+source process_noData.sh yield_AN_sr_sys_0830_JER_Down no JER_Down no no
+source process_noData.sh yield_AN_sr_sys_0830_JES_Up no JES_Up no no
+source process_noData.sh yield_AN_sr_sys_0830_JES_Down no JES_Down no no
+source process_noData.sh yield_AN_sr_sys_0830_JMR_Up no JMR_Up no no
+source process_noData.sh yield_AN_sr_sys_0830_JMR_Down no JMR_Down no no
+source process_noData.sh yield_AN_sr_sys_0830_JMS_Up no JMS_Up no no
+source process_noData.sh yield_AN_sr_sys_0830_JMS_Down no JMS_Down no no
+source process_noData.sh yield_AN_sr_sys_0830_nominal yes nominal no yes
+#source process_noData.sh yield_AN_sr_sys_0830_trig_nominal yes nominal yes no
