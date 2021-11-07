df4f91c3b2485b71c5c758b8b9c75e06d63f5a62
Irene Dutta <irene.dutta@cern.ch>
2021-11-07 14:27:12 -0800

    add condor resubmission

--- condor/makeCondorSubmit.py
+++ condor/makeCondorSubmit.py
@@ -21,23 +21,30 @@ datasets = ["qcd",
 "others"
 ]
 Tags =[
-    ["yield_AN_sr_sys_0830_nominal_nosys","no","nominal","no","no"],
-    ["yield_AN_sr_sys_0830_JER_Up","no","JER_Up","no","no"],
-    ["yield_AN_sr_sys_0830_JER_Down","no","JER_Down","no","no"],
-    ["yield_AN_sr_sys_0830_JES_Up","no","JES_Up","no","no"],
-    ["yield_AN_sr_sys_0830_JES_Down","no","JES_Down","no","no"],
-    ["yield_AN_sr_sys_0830_JMR_Up","no","JMR_Up","no","no"],
-    ["yield_AN_sr_sys_0830_JMR_Down","no","JMR_Down","no","no"],
-    ["yield_AN_sr_sys_0830_JMS_Up","no","JMS_Up","no","no"],
-    ["yield_AN_sr_sys_0830_JMS_Down","no","JMS_Down","no","no"],
-    ["yield_AN_sr_sys_0830_nominal","yes","nominal","no","yes"],
-    #["yield_AN_sr_sys_0830_trig_nominal","yes","nominal","yes","no"],
+    ["yield_AN_sr_sys_1105_nominal_nosys","no","nominal","no","no"],
+    ["yield_AN_sr_sys_1105_JER_Up","no","JER_Up","no","no"],
+    ["yield_AN_sr_sys_1105_JER_Down","no","JER_Down","no","no"],
+    ["yield_AN_sr_sys_1105_JES_Up","no","JES_Up","no","no"],
+    ["yield_AN_sr_sys_1105_JES_Down","no","JES_Down","no","no"],
+    ["yield_AN_sr_sys_1105_JMR_Up","no","JMR_Up","no","no"],
+    ["yield_AN_sr_sys_1105_JMR_Down","no","JMR_Down","no","no"],
+    ["yield_AN_sr_sys_1105_JMS_Up","no","JMS_Up","no","no"],
+    ["yield_AN_sr_sys_1105_JMS_Down","no","JMS_Down","no","no"],
+    ["yield_AN_sr_sys_1105_nominal","yes","nominal","no","yes"],
+    #["yield_AN_sr_sys_1105_trig_nominal","yes","nominal","yes","no"],
 ]
 inputBase = "/storage/af/user/nlu/work/HH/ntuples/20210712_regression_v2/option5/combined/BDT/"
 
 for data in datasets:
     for year in ["2016","2017","2018"]:
+    #for year in ["2016"]:
         for tag in Tags:
+            condorout = "mkdir /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/condor/condor_output/"+tag[0]+"_"+data
+            os.system(condorout)
+            cpdata = "cp -r /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/data /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/condor/condor_output/"+tag[0]+"_"+data
+            cppro = "cp /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/HHLooper /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/condor/condor_output/"+tag[0]+"_"+data
+            os.system(cpdata)
+            os.system(cppro)
             condorSubmit = "condor_submit/submitCondor_"+data+"_"+tag[0]+"_"+year
             jobName      = str(date.today())+"_"+data+"_"+tag[0]+"_"+year
             outHistFile = data+".root"
@@ -46,6 +53,11 @@ for data in datasets:
             else:
                 isData       =  "0"
             inputDir = inputBase+year+"/"+data
+            print(tag,data)
+            if "trig" in tag[0] and year == "2016":
+                if data == "ttbar" or data == "others" or data == "HHVBF0p511" or data == "HHVBF1p511" or data == "HHVBF121" or data == "HHVBF101":
+                    inputDir = inputBase+year+"/"+data+"/skim"
+            print(inputDir) 
             shutil.copyfile("proto_condor_submit",condorSubmit)
             for line in fileinput.FileInput(condorSubmit, inplace=1):
                 line=line.replace("JOBNAME", jobName)
@@ -64,8 +76,8 @@ for data in datasets:
             os.system(submitCommand)
 
 for year in ["2016","2017","2018"]:
-    condorSubmit = "condor_submit/submitCondor_data_yield_AN_sr_sys_0830_nominal_nosys_"+year
-    jobName      = str(date.today())+"_data_yield_AN_sr_sys_0830_nominal_nosys_"+year
+    condorSubmit = "condor_submit/submitCondor_data_yield_AN_sr_sys_1105_nominal_nosys_"+year
+    jobName      = str(date.today())+"_data_yield_AN_sr_sys_1105_nominal_nosys_"+year
     outHistFile = "data.root"
     isData       =  "1"
     inputDir = inputBase+year+"/data"
@@ -74,7 +86,7 @@ for year in ["2016","2017","2018"]:
         line=line.replace("JOBNAME", jobName)
         line=line.replace("INPUTDIR",inputDir)
         line=line.replace("OUTFILENAME",outHistFile)
-        line=line.replace("TAG","yield_AN_sr_sys_0830_nominal_nosys")
+        line=line.replace("TAG","yield_AN_sr_sys_1105_nominal_nosys")
         line=line.replace("ISDATA",isData)
         line=line.replace("DOSYST","no")
         line=line.replace("DOSHAPESYST","nominal")
@@ -84,5 +96,5 @@ for year in ["2016","2017","2018"]:
                         
     submitCommand = "condor_submit "+condorSubmit
     print submitCommand
-    os.system(submitCommand)
+    #os.system(submitCommand)
     
--- condor/run_myprog.sh
+++ condor/run_myprog.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-cd /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/condor/condor_output/condor_logs
+v=$2
+proc=${v::-5}
+#mkdir /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/condor/condor_output/$3_$proc
+cd /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/condor/condor_output/$3_$proc
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scram runtime -sh`
 #cd $TMP
 cp -r /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/data .
-echo "/storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/HHLooper $1 $2 $3 $4 $5 $6 $7 $8"
-/storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/HHLooper $1 $2 $3 $4 $5 $6 $7 $8
+cp /storage/af/user/idutta/HH/CMSSW_9_4_2/src/V2/HHLooper/HHLooper .
+echo "./HHLooper $1 $2 $3 $4 $5 $6 $7 $8"
+./HHLooper $1 $2 $3 $4 $5 $6 $7 $8
 
 #$1 -  ${inputBase}/${year}/${process}
 #$2 - outputfilename
